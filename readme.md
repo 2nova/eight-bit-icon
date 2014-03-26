6b33a3e17576070c5f742348b081092519eaef20
Ivan Nikitin <vantoozz@gmail.com>
2014-03-26 15:09:57 +0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# 8-bit icon for PHP
+
+Icons generator based on http://8biticon.com/ images
---
+++ src/Novanova/EightBitIcon/EightBitIcon.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Novanova\EightBitIcon;
+
+use Gregwar\Image\Image;
+
+class EightBitIcon
+{
+
+    private $size = 400;
+
+    public function generate($filename, $sex = null, $unfiltered = false)
+    {
+        $DS = DIRECTORY_SEPARATOR;
+
+        $parts = array('background', 'face', 'clothes', 'hair', 'eyes', 'mouth');
+
+        if (!in_array($sex, array('male', 'female'))) {
+            $sex = 1 === mt_rand(1, 2) ? 'male' : 'female';
+        }
+
+        $img = Image::create($this->size, $this->size);
+
+        foreach ($parts as $part) {
+            $images = glob(__DIR__ . $DS . 'img' . $DS . $sex . $DS . $part . $DS .'*.{jpg,jpeg,png,gif}', GLOB_BRACE);
+
+            if (!$unfiltered) {
+                $images = array_filter(
+                    $images,
+                    function ($image) {
+                        $path_parts = pathinfo($image);
+                        return false === stristr($path_parts['filename'], 'unfiltered-');
+                    }
+                );
+            }
+            $img->merge(Image::open($images[array_rand($images)]);
+        }
+
+        $img->save($filename);
+    }
+
+}
